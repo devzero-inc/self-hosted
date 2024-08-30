9f87d2bfa7371e63c61773e2cde6dd4c7e384f3f
MauroKinderknecht <56045994+MauroKinderknecht@users.noreply.github.com>
2024-08-29 22:57:38 -0300

    create cluster in polland

--- install.sh
+++ install.sh
@@ -19,7 +19,7 @@ if [ "$OS" = "Darwin" ]; then
     echo "Homebrew is not installed. Please install Homebrew first."
     exit 1
   fi
-  brew install qemu jq lima make kubectl
+  brew install qemu jq lima make kubectl yq
 elif [ "$OS" = "Linux" ]; then
   # id -u devzero &>/dev/null || sudo useradd -m -s /bin/bash devzero && sudo usermod -aG sudo devzero && echo "devzero ALL=(ALL) NOPASSWD:ALL" | sudo tee /etc/sudoers.d/devzero
   sudo apt-get install qemu-system jq make -y
@@ -38,58 +38,42 @@ echo "Dependencies installed."
 # Copy credentials to the correct location
 echo "Copying credentials..."
 mkdir -p ~/.lima ~/.lima/_config
-curl -fsSL https://raw.githubusercontent.com/devzero-inc/self-hosted/main/keys/user -o ~/.lima/_config/user
-curl -fsSL https://raw.githubusercontent.com/devzero-inc/self-hosted/main/keys/user.pub -o ~/.lima/_config/user.pub
+cp -r ./keys ~/.lima/_config
 chmod 400 ~/.lima/_config/user
 echo "Credentials copied."
-
-cat <<'EOF' >dz_cluster.yaml
-vmType: "qemu"
-cpus: 6
-memory: "10GiB"
-disk: "100GiB"
-images:
-  - location: "https://self-hosted.devzero.io/dz-cluster-base-arm64.qcow2"
-    arch: "aarch64"
-  - location: "https://self-hosted.devzero.io/dz-cluster-base-amd64.qcow2"
-    arch: "x86_64"
-mounts: []
-provision:
-  - mode: user
-    script: |
-      #!/bin/bash
-      set -eux -o pipefail
-      sudo hostnamectl set-hostname dz-cluster
-      minikube start --driver=none --container-runtime=cri-o --kubernetes-version=v1.29.0 --cni=bridge --embed-certs
-probes:
-  - script: |
-      #!/bin/bash
-      set -eux -o pipefail
-      output=$(kubectl get pods -A)
-
-      # Check if the output is empty
-      if [ -z "$output" ]; then
-        echo "No pods found!"
-        exit 1
-      fi
-
-      # # Check if any pod is not in Running status
-      # if echo "$output" | grep -v -E 'Running|STATUS'; then
-      #   echo "One or more pods are not in Running status!"
-      #   exit 1
-      # fi
-
-      echo "All pods are in Running status."
-      exit 0
-EOF
 
 # Start the VM
 echo "Starting the cluster..."
 limactl create dz_cluster.yaml --tty=false
 limactl start dz_cluster
 limactl shell dz_cluster -- cat .kube/config | tail -n +2 | sed -e 's|server:.*|server: https://127.0.0.1:8443|' > kubeconfig
-kubectl get secret devzero-sa0-token -n default -o jsonpath='{.data.token}' --kubeconfig kubeconfig| base64 -d > sa-token
 echo "Data plane started."
 
 # Install control plane
 /bin/bash ./dz/run.sh
+
+# Create cluster in polland
+certificate_authority_data=$(cat kubeconfig | yq e '.clusters[0].cluster."certificate-authority-data"' -)
+token=$(kubectl get secret devzero-sa0-token -n default -o jsonpath='{.data.token}' --kubeconfig kubeconfig| base64 -d)
+
+docker compose run polland ./manage.py shell_plus -c 'user = User.objects.get(email="devzero@devzero.io"); user.set_password("123123"); user.save();'
+docker compose run polland ./manage.py shell_plus -c "cluster, created = Cluster.objects.get_or_create(
+  cluster_id=1,
+  defaults={
+      'name': 'minikube',
+      'certificate_authority_data': \"$certificate_authority_data\",
+      'server': 'https://host.docker.internal:8443',
+      'service_account_name': 'devzero-sa0',
+      'service_account_token': \"$token\",
+      'slug': 'minikube'
+  }
+)
+if not created:
+  # Update the existing cluster with new values
+  cluster.name = 'minikube'
+  cluster.certificate_authority_data = \"$certificate_authority_data\"
+  cluster.server = 'https://host.docker.internal:8443'
+  cluster.service_account_name = 'devzero-sa0'
+  cluster.service_account_token = \"$token\"
+  cluster.slug = 'minikube'
+  cluster.save()"
