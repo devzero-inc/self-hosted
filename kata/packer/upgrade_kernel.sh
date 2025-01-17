a9b60e95509dafa33148246bf43da5b4793b4960
garvit3835 <97427673+garvit3835@users.noreply.github.com>
2025-01-17 21:56:15 +0530

    Update upgrade_kernel.sh

--- kata/packer/upgrade_kernel.sh
+++ kata/packer/upgrade_kernel.sh
@@ -7,6 +7,25 @@ echo "Upgrading kernel!"
 # Install the downloaded packages
 echo "Installing kernel packages..."
 yum localinstall -y /tmp/*.rpm
+grubby --info=ALL | grep "vmlinuz"
+ls -l /boot/vmlinuz-6.7.0-rc6+
+
+
+ls -al /boot
+
+sudo find / -type f -name "vmlinuz-6.7.0-rc6+" 2>/dev/null
+sudo find / -type f -name "vmlinuz-6.7.0-rc6+" 2>/dev/null
+sudo find / -type f -name "vmlinuz-6.7.0-rc6+" 2>/dev/null
+sudo find / -type f -name "vmlinuz-6.7.0-rc6+" 2>/dev/null
+
+sudo find /boot /lib/modules /usr/src /var/tmp -type f -name "vmlinuz*" 2>/dev/null
+
+
+
+grubby --info=ALL
+
+sudo grubby --add-kernel=/boot/vmlinuz-6.7.0-rc6+ --title "Amazon Linux (6.7.0-rc6+)"
+
 
 grubby --set-default /boot/vmlinuz-6.7.0-rc6+
 grubby --args="quiet splash nokaslr pti=off console=tty1 console=ttyS0 net.ifnames=0 biosdevname=0 nvme_core.io_timeout=4294967295 rd.emergency=poweroff rd.shell=0" --update-kernel /boot/vmlinuz-6.7.0-rc6+
@@ -20,3 +39,5 @@ GRUB_TERMINAL="ec2-console console serial"
 GRUB_SERIAL_COMMAND="serial --unit=0 --speed=115200 --word=8 --parity=no --stop=1"
 EOF
 grub2-mkconfig -o /boot/grub2/grub.cfg
+
+
