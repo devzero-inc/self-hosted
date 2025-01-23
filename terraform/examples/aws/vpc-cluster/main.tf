83c73f71acb5ce29e974949c371c5e60c4c2459f
Everton Moreth <everton.moreth@devzero.io>
2025-01-23 15:46:30 -0300

    Lint cleanup and changes

--- terraform/examples/aws/vpc-cluster/main.tf
+++ terraform/examples/aws/vpc-cluster/main.tf
@@ -1,13 +1,13 @@
 locals {
   azs = (var.availability_zones_count > 0) ? slice(data.aws_availability_zones.available.names, 0, min(var.availability_zones_count, length(data.aws_availability_zones.available.names))) : var.availability_zones
 
-  calculated_public_subnets_ids = length(var.public_subnet_ids) > 0 ? var.public_subnet_ids : module.vpc.public_subnets
+  calculated_public_subnets_ids  = length(var.public_subnet_ids) > 0 ? var.public_subnet_ids : module.vpc.public_subnets
   calculated_private_subnets_ids = length(var.private_subnet_ids) > 0 ? var.private_subnet_ids : module.vpc.private_subnets
-  calculated_security_group_ids = length(var.security_group_ids) > 0 ? var.security_group_ids : [module.vpc.default_security_group_id]
+  calculated_security_group_ids  = length(var.security_group_ids) > 0 ? var.security_group_ids : [module.vpc.default_security_group_id]
 
   vpc_id = var.create_vpc ? module.vpc.vpc_id : var.vpc_id
 
-  calculated_public_subnets_cidrs = [for k, v in local.azs : cidrsubnet(var.cidr, 4, k)]
+  calculated_public_subnets_cidrs  = [for k, v in local.azs : cidrsubnet(var.cidr, 4, k)]
   calculated_private_subnets_cidrs = [for k, v in local.azs : cidrsubnet(var.cidr, 4, k + 6)]
 
   private_subnet_cidr_blocks = var.create_vpc ? module.vpc.private_subnets_cidr_blocks : [for subnet in data.aws_subnet.private_subnets : subnet.cidr_block]
@@ -18,14 +18,30 @@ locals {
 
   effective_vpc_cidr_block = var.create_vpc ? module.vpc.vpc_cidr_block : data.aws_vpc.existing[0].cidr_block
 
-  vpc_dns_resolver = cidrhost(local.effective_vpc_cidr_block, 2) # Calculates the +2 host of the CIDR for VPN DNS resolving
+  vpc_dns_resolver = cidrhost(local.effective_vpc_cidr_block, 2)
+  # Calculates the +2 host of the CIDR for VPN DNS resolving
 
-  static_node_groups = {
-    "node_group_1" = module.eks.eks_managed_node_groups["${var.cluster_name}-nodes"].node_group_autoscaling_group_names[0]
-  }
+  #   static_node_groups = {
+  #     "node_group_1" = module.eks.eks_managed_node_groups["${var.cluster_name}-nodes"].node_group_autoscaling_group_names[0]
+  #   }
 }
 
 data "aws_availability_zones" "available" {}
+
+data "aws_caller_identity" "current" {
+  count = var.add_current_user_to_kms ? 1 : 0
+}
+
+data "aws_iam_session_context" "current" {
+  count = var.add_current_user_to_kms ? 1 : 0
+
+  # This data source provides information on the IAM source role of an STS assumed role
+  # For non-role ARNs, this data source simply passes the ARN through issuer ARN
+  # Ref https://github.com/terraform-aws-modules/terraform-aws-eks/issues/2327#issuecomment-1355581682
+  # Ref https://github.com/hashicorp/terraform-provider-aws/issues/28381
+  arn = try(data.aws_caller_identity.current[0].arn, "")
+}
+
 
 ################################################################################
 # Providers
@@ -38,12 +54,12 @@ provider "aws" {
 }
 
 data "aws_eks_cluster" "cluster-data" {
-  name = module.eks.cluster_name
+  name       = module.eks.cluster_name
   depends_on = [module.eks]
 }
 
 data "aws_eks_cluster_auth" "cluster-auth" {
-  name = module.eks.cluster_name
+  name       = module.eks.cluster_name
   depends_on = [module.eks]
 }
 
@@ -102,31 +118,6 @@ resource "null_resource" "validations" {
 
 
 ################################################################################
-# VPN
-################################################################################
-
-module "vpn" {
-  count = var.create_vpn ? 1 : 0
-
-  source = "../../../modules/aws/vpn"
-
-  name                          = var.cluster_name
-  region                        = var.region
-  additional_security_group_ids = [module.eks.cluster_primary_security_group_id]
-  vpc_id                        = local.vpc_id
-  subnet_ids                    = local.calculated_private_subnets_ids
-  client_vpn_cidr_block         = var.client_vpn_cidr_block
-  vpc_dns_resolver              = local.vpc_dns_resolver
-
-  vpn_client_list               = var.vpn_client_list
-
-  additional_server_dns_names = [
-    "${var.domain}",
-    "*.${var.domain}"
-  ]
-}
-
-################################################################################
 # VPC
 ################################################################################
 data "aws_subnet" "private_subnets" {
@@ -161,44 +152,32 @@ module "vpc" {
   public_subnets  = local.calculated_public_subnets_cidrs
   private_subnets = local.calculated_private_subnets_cidrs
 
-  public_subnet_tags  = var.additional_public_subnet_tags
-  private_subnet_tags = var.additional_private_subnet_tags
-
-  # network acl
-  manage_default_network_acl = var.manage_default_network_acl
-
   # nat gateways
   enable_nat_gateway     = var.enable_nat_gateway
   single_nat_gateway     = var.single_nat_gateway
   one_nat_gateway_per_az = var.one_nat_gateway_per_az
 
-  propagate_private_route_tables_vgw = false
-  propagate_public_route_tables_vgw  = false
-
   # internet gateway
-  create_igw       = var.create_igw
-  instance_tenancy = var.instance_tenancy
+  create_igw = var.create_igw
 
   # enable vpn gateway
   enable_vpn_gateway = var.create_vpn
 
-  enable_dns_hostnames = true
-  enable_dns_support = true
-  enable_dhcp_options = true
+  enable_dhcp_options = var.enable_dhcp_options
 
   default_security_group_egress = [
     {
-      from_port = 0
-      to_port   = 0
-      protocol  = "-1"
+      from_port   = 0
+      to_port     = 0
+      protocol    = "-1"
       cidr_blocks = "0.0.0.0/0"
     }
   ]
   default_security_group_ingress = [
     {
-      from_port = 0
-      to_port   = 0
-      protocol  = "-1"
+      from_port   = 0
+      to_port     = 0
+      protocol    = "-1"
       cidr_blocks = join(",", local.calculated_private_subnets_cidrs)
     }
   ]
@@ -209,42 +188,6 @@ module "vpc" {
 ################################################################################
 # EKS
 ################################################################################
-data "aws_ami" "ubuntu-eks_1_30" {
-  name_regex  = "ubuntu-eks/k8s_1.30/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"
-  most_recent = true
-  owners      = ["099720109477"]
-}
-
-module "node_cluster_role" {
-  source  = "terraform-aws-modules/iam/aws//modules/iam-assumable-role"
-  version = "5.51.0"
-
-  trusted_role_services = [
-    "ec2.amazonaws.com",
-  ]
-  trusted_role_actions = [
-    "sts:AssumeRole",
-  ]
-
-  create_role       = true
-  role_name_prefix  = "${substr(var.cluster_name,0 ,(38-length(var.node_role_suffix)))}${var.node_role_suffix}"
-  role_description  = "EKS managed node group IAM role"
-  role_requires_mfa = false
-
-  force_detach_policies = true
-
-  custom_role_policy_arns = [
-    "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore",
-    "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy",
-    "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly",
-    "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy",
-  ]
-}
-
-#data "aws_iam_roles" "sso_awsadministratoraccess" {
-#  name_regex = "AWSReservedSSO_AWSAdministratorAccess.*"
-#}
-
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "20.31.6"
@@ -259,7 +202,9 @@ module "eks" {
 
   cluster_endpoint_public_access       = var.cluster_endpoint_public_access
   cluster_endpoint_public_access_cidrs = var.cluster_endpoint_public_access ? var.cluster_endpoint_public_access_cidrs : []
-  cluster_endpoint_private_access      = true # TODO: we should check this a bit more, private should always be enabled? 
+
+  # We always enable private access by default.
+  cluster_endpoint_private_access = true
 
   cluster_additional_security_group_ids = var.security_group_ids
 
@@ -268,65 +213,20 @@ module "eks" {
   ]
 
   kms_key_administrators = concat(
-    var.kms_key_administrators, 
-    #[
-    #  one(data.aws_iam_roles.sso_awsadministratoraccess.arns)
-    #]
+    var.kms_key_administrators,
+    [
+      try(data.aws_iam_session_context.current[0].issuer_arn, "")
+    ]
   )
+
+  # We need this to create an initial role to be added to KMS key
+  create_iam_role = true
+  # Hack to help fixing length the name of the iam role for long named clusters
+  iam_role_name = "${substr(var.cluster_name, 0, (37 - length("-cluster")))}-cluster"
+
 
   kms_key_aliases               = [var.cluster_name]
   kms_key_enable_default_policy = var.kms_key_enable_default_policy
-
-  eks_managed_node_groups = {
-    "${var.cluster_name}-nodes" = {
-      name           = "${var.cluster_name}-nodes"
-      instance_types = [var.worker_instance_type]
-      key_name       = var.nodes_key_name
-
-      ami_id = data.aws_ami.ubuntu-eks_1_30.image_id
-
-      create_iam_role = false
-      iam_role_arn    = module.node_cluster_role.iam_role_arn
-
-      min_size     = var.desired_node_size
-      max_size     = var.max_node_size
-      desired_size = var.desired_node_size
-
-      enable_bootstrap_user_data = true
-
-      post_bootstrap_user_data = <<-EOT
-          #!/bin/bash
-          set -o xtrace
-          # Backup the original config.toml
-          cp /etc/containerd/config.toml /etc/containerd/config.toml.bak
-
-          echo '' >> /etc/containerd/config.toml
-          echo '[plugins."io.containerd.grpc.v1.cri".registry.configs."docker-registry.devzero.svc.cluster.local:5000".tls]'  >> /etc/containerd/config.toml
-          echo '  insecure_skip_verify = true'  >> /etc/containerd/config.toml
-
-          # Restart containerd to apply the changes
-          systemctl restart containerd
-      EOT
-
-      bootstrap_extra_args       = "--kubelet-extra-args '--runtime-request-timeout=\"15m\"'"
-
-      block_device_mappings = {
-        sda = {
-          device_name = "/dev/sda1"
-          ebs = {
-            delete_on_termination = true
-            encrypted             = true
-            volume_size           = 500
-            volume_type           = "gp3"
-          }
-        }
-      }
-
-      update_config = {
-        max_unavailable_percentage = 33
-      }
-    }
-  }
 
   cluster_identity_providers = var.cluster_identity_providers
 
@@ -340,8 +240,49 @@ module "eks" {
   tags = var.tags
 }
 
-# Data source to get the AWS account ID
-data "aws_caller_identity" "current" {}
+
+module "ubuntu_node_group" {
+  source = "../../../modules/aws/ubuntu_node_group"
+
+  cluster_name = module.eks.cluster_name
+
+  instance_type = var.instance_type
+
+  desired_size = var.desired_size
+  min_size     = var.min_size
+  max_size     = var.max_size
+
+  depends_on = [
+    module.eks
+  ]
+}
+
+
+################################################################################
+# VPN
+################################################################################
+
+module "vpn" {
+  count = var.create_vpn ? 1 : 0
+
+  source = "../../../modules/aws/vpn"
+
+  name                          = var.cluster_name
+  region                        = var.region
+  additional_security_group_ids = [module.eks.cluster_primary_security_group_id]
+  vpc_id                        = local.vpc_id
+  subnet_ids                    = local.calculated_private_subnets_ids
+  client_vpn_cidr_block         = var.client_vpn_cidr_block
+  vpc_dns_resolver              = local.vpc_dns_resolver
+
+  vpn_client_list = var.vpn_client_list
+
+  additional_server_dns_names = [
+    "${var.domain}",
+    "*.${var.domain}"
+  ]
+}
+
 
 ################################################################################
 # Ingress configuration
@@ -362,34 +303,33 @@ resource "aws_route53_zone" "private" {
 ################################################################################
 # Example of using custom ALB, and pointing it to the cluster node port
 ################################################################################
-
-#module "alb" {
-#  source = "../../../modules/aws/alb"
+# module "alb" {
+#   source = "../../../modules/aws/alb"
+#   count  = var.create_alb ? 1 : 0
 #
-#  name               = "${var.name}-backend"
+#   name = "${var.cluster_name}-service"
 #
-#  node_group_asg_names = local.static_node_groups
+#   node_group_asg_names = local.static_node_groups
 #
-#  additional_security_group_ids = [module.eks.node_security_group_id]
-#  vpc_id                        = local.vpc_id
-#  subnet_ids                    = local.calculated_private_subnets_ids
-#  vpc_cidr                      = local.effective_vpc_cidr_block
-#  certificate_arn               = module.vpn[0].vpn_server_certificate_arn
-#  target_port                   = 30080
-#  record                        = "backend.${var.domain}"
-#  zone_id                       = local.effective_zone_id
-#  health_check = {
-#    enabled             = true
-#    path                = "/"       
-#    interval            = 30     
-#    timeout             = 5        
-#    healthy_threshold   = 2       
-#    unhealthy_threshold = 2          
-#    matcher             = "200"     
-#  }
+#   additional_security_group_ids = [module.eks.node_security_group_id]
+#   vpc_id          = local.vpc_id
+#   subnet_ids      = local.calculated_private_subnets_ids
+#   vpc_cidr        = local.effective_vpc_cidr_block
+#   certificate_arn = var.create_vpn ? module.vpn[0].vpn_server_certificate_arn : null
+#   target_port     = 30080
+#   record          = "service.${var.domain}"
+#   zone_id         = local.effective_zone_id
+#   health_check = {
+#     enabled             = true
+#     path                = "/"
+#     interval            = 30
+#     timeout             = 5
+#     healthy_threshold   = 2
+#     unhealthy_threshold = 2
+#     matcher             = "200"
+#   }
 #
-#  depends_on = [
-#    module.vpn,
-#    module.eks
-#  ]
-#}
+#   depends_on = [
+#     module.eks
+#   ]
+# }
