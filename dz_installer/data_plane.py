6112cc58b78e6bed4b18fd381e7d15d6b2c309ca
garvit3835 <garvit3835@gmail.com>
2025-01-30 22:25:46 +0530

    cli function: dzi check data-plane rook-ceph

--- dz_installer/data_plane.py
+++ dz_installer/data_plane.py
@@ -32,3 +32,35 @@ class DataPlane:
 
         cfg.save()
         success("Data Plane Prometheus checks")
+
+
+    @staticmethod
+    def check_data_plane_rook_ceph(force):
+        """Check if the Rook-Ceph Helm chart is installed, check its version, and store the status in the CLI state file."""
+        info("Checking Rook-Ceph installation in the data plane...")
+
+        chart_name = "rook-ceph"
+        cfg = DZConfig().data
+
+        if cfg.data_plane.rook_ceph_installed and not force:
+            click.echo("Rook-Ceph is already installed. Skipping check...")
+            return
+
+        try:
+            if check_chart_is_installed(chart_name):
+                success("Rook-Ceph chart is installed.")
+                cfg.data_plane.rook_ceph_installed = True
+            else:
+                click.echo("Rook-Ceph chart is not installed.")
+                install = click.confirm("Do you want to install Rook-Ceph?", default=True)
+                if not install:
+                    click.echo("Cannot proceed without Rook-Ceph.")
+                    return
+        except RuntimeError as e:
+            error(f"Error checking Rook-Ceph chart: {e}")
+            return
+
+        cfg.save()
+        success("Data Plane Rook-Ceph checks")
+
+
