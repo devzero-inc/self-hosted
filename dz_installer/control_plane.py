88477529ea6b13b1cd8d0927c20df4f36a115a92
Mauro Kinderknecht <mauro.mkinderknecht@gmail.com>
2025-02-04 14:04:57 -0300

    add dzi commands for control plane

--- dz_installer/control_plane.py
+++ dz_installer/control_plane.py
@@ -5,13 +5,16 @@ import click
 from kubernetes import client, config
 from kubernetes.client import V1IngressClass
 from ruamel.yaml import YAML
-from dz_installer.dz_config import DZConfig
+from dz_installer.dz_config import DZConfig, DZDotMap
 from dz_installer.helpers import error, success, info, check_chart_is_installed
 
 yaml = YAML()
 yaml.preserve_quotes = True
 
 config.load_kube_config()
+
+control_plane_deps_dir = pathlib.Path("./charts/dz-control-plane-deps")
+control_plane_dir = pathlib.Path("./charts/dz-control-plane")
 
 class ControlPlane:
 
@@ -26,12 +29,11 @@ class ControlPlane:
             if check_chart_is_installed("dz-control-plane"):
                 success("Control plane is installed")
                 if not force:
-                    return False
+                    return
             else:
                 click.echo("Control plane is not installed")
         except RuntimeError as e:
             self.error(str(e))
-            return False
 
         globals_cfg = DZConfig().data.globals
         control_plane_cfg = DZConfig().data.control_plane
@@ -86,21 +88,22 @@ class ControlPlane:
         # config.data_plane_arch = data_plane_arch
         # config.save()
 
+        control_plane_cfg.ready = True
+        control_plane_cfg.save()
+
         success("Control plane is ready to be installed")
-        return True
 
-    def install_control_plane_chart(self, force):
-        can_install = self.check_control_plane_chart(force)
-
-        if not can_install and not force:
-            return
-
-        info("Installing control plane...")
-
+    def install_control_plane_chart(self):
         globals_cfg = DZConfig().data.globals
         control_plane_cfg = DZConfig().data.control_plane
 
-        file = pathlib.Path("./charts/dz-control-plane/values.yaml")
+        if not control_plane_cfg.ready:
+            click.echo("Control plane is not ready to be installed. Please run checks first")
+            self.error("NOT_READY")
+
+        info("Installing control plane...")
+
+        file = pathlib.Path(f"{control_plane_dir}/values.yaml")
         values = yaml.load(file)
 
         click.echo()
@@ -119,14 +122,10 @@ class ControlPlane:
                 values['credentials']['password'] = control_plane_cfg.docker_hub.password
                 values['credentials']['email'] = control_plane_cfg.docker_hub.email
 
-        # TODO: handle provisioning dbs in rds
-        # if control_plane_cfg.has_attr("provision_dbs_in_cluster"):
-        #     ...
-
         yaml.dump(values, file)
 
         try:
-            sh.helm("upgrade", "--install", "devzero", "./charts/dz-control-plane", "-n", "devzero", "--create-namespace")
+            sh.make("install", _cwd=control_plane_dir)
         except sh.ErrorReturnCode as err:
             click.echo(f"Error installing control plane: {err.stderr.decode('utf-8')}", err=True)
             self.error("INSTALL_FAILED")
@@ -161,7 +160,51 @@ class ControlPlane:
         if not control_plane_cfg.ingress.install and not control_plane_cfg.ingress.cls:
             click.echo("Cannot proceed without an ingress class")
             self.error("INGRESS_CLASS_NOT_FOUND")
-        success("Control plane ingress checks")
+        success("Control plane ingress checks done")
+
+    def install_control_plane_ingress(self):
+        control_plane_cfg = DZConfig().data.control_plane
+
+        globals_cfg = DZConfig().data.globals
+
+        if not control_plane_cfg.ingress.cls and not control_plane_cfg.ingress.install:
+            click.echo("Ingress checks missing. Please run checks first")
+            return
+
+        if control_plane_cfg.ingress.install or control_plane_cfg.ingress.cls == "new":
+            file = pathlib.Path(f"{control_plane_deps_dir}/values/ingress-nginx.yaml")
+            values = yaml.load(file)
+
+            annotations = values['controller']['service']['annotations']
+
+            if globals_cfg.provider == "aws":
+                aws_cfg = DZConfig().data.aws
+                subnets = aws_cfg.vpc.subnets.keys()
+
+                public = False
+                for subnet in subnets:
+                    if aws_cfg.vpc.subnets[subnet].public:
+                        public = True
+                        break
+
+                if not public:
+                    annotations['service.beta.kubernetes.io/aws-load-balancer-scheme'] = "internal"
+                    annotations['service.beta.kubernetes.io/aws-load-balancer-subnets'] = ",".join(subnets)
+                    annotations['service.beta.kubernetes.io/aws-load-balancer-backend-protocol'] = "ssl"
+                    annotations['service.beta.kubernetes.io/aws-load-balancer-ssl-ports'] = "https, http"
+
+                if control_plane_cfg.cert_manager.external:
+                    annotations['service.beta.kubernetes.io/aws-load-balancer-ssl-cert'] = control_plane_cfg.cert_manager.cert_arn
+
+            yaml.dump(values, file)
+
+            try:
+                sh.make("install-ingress-nginx", _cwd=control_plane_deps_dir)
+            except sh.ErrorReturnCode as err:
+                click.echo(f"Error installing ingress controller: {err.stderr.decode('utf-8')}", err=True)
+                self.error("INGRESS_INSTALL_FAILED")
+
+    success("Control plane ingress installed successfully")
 
     def check_control_plane_cert_manager(self, force):
         info("Checking control plane certificates...")
@@ -205,4 +248,81 @@ class ControlPlane:
             control_plane_cfg.cert_manager.issuer = click.prompt("Please select an existing ClusterIssuer for the control plane. Use new if you want devzero to create a new ClusterIssuer", type=click.Choice(issuers), default=issuers[0])
 
         control_plane_cfg.save()
-        success("Control plane certificates checks")
+        success("Control plane certificates checks done")
+
+    def install_control_plane_cert_manager(self):
+        control_plane_cfg = DZConfig().data.control_plane
+
+        if not control_plane_cfg.cert_manager.install and not control_plane_cfg.cert_manager.external and not control_plane_cfg.cert_manager.issuer:
+            click.echo("Certificate checks missing. Please run checks first")
+            self.error("NOT_READY")
+
+        if control_plane_cfg.cert_manager.install:
+            try:
+                sh.make("install-cert-manager", _cwd=control_plane_deps_dir)
+            except sh.ErrorReturnCode as err:
+                click.echo(f"Error installing cert-manager: {err.stderr.decode('utf-8')}", err=True)
+                self.error("CERT_MANAGER_INSTALL_FAILED")
+
+        if control_plane_cfg.cert_manager.issuer == "new":
+            file = pathlib.Path(f"{control_plane_deps_dir}/values/cluster-issuer.yaml")
+            values = yaml.load(file)
+
+            values['spec']['acme']['solvers'][0]['http01']['ingress']['class'] = control_plane_cfg.ingress.cls if control_plane_cfg.ingress.cls not in ["new", ""] else "nginx"
+
+            yaml.dump(values, file)
+
+            try:
+                sh.kubectl("apply", "-f", file)
+            except sh.ErrorReturnCode as err:
+                click.echo(f"Error creating ClusterIssuer: {err.stderr.decode('utf-8')}", err=True)
+                self.error("CLUSTER_ISSUER_CREATE_FAILED")
+
+    success("Control plane certificates installed successfully")
+
+    def install_control_plane_deps(self):
+        control_plane_cfg = DZConfig().data.control_plane
+        global_cfg = DZConfig().data.globals
+
+        if control_plane_cfg.provision_dbs_in_cluster:
+            try:
+                sh.make("install-mysql-pulse", _cwd=control_plane_deps_dir)
+                sh.make("install-mongodb", _cwd=control_plane_deps_dir)
+                sh.make("install-redis", _cwd=control_plane_deps_dir)
+                sh.make("install-postgres-logsrv", _cwd=control_plane_deps_dir)
+                sh.make("install-postgres-hydra", _cwd=control_plane_deps_dir)
+                sh.make("install-postgres-polland", _cwd=control_plane_deps_dir)
+                sh.make("install-postgres-vault", _cwd=control_plane_deps_dir)
+                sh.make("install-timescaledb-single", _cwd=control_plane_deps_dir)
+                sh.make("install-elasticmq", _cwd=control_plane_deps_dir)
+            except sh.ErrorReturnCode as err:
+                click.echo(f"Error installing databases: {err.stderr.decode('utf-8')}", err=True)
+                self.error("DATABASES_INSTALL_FAILED")
+
+        try:
+            deps = ["registry", "grafana", "mimir", "vault"]
+
+            for dep in deps:
+                file = pathlib.Path(f"{control_plane_deps_dir}/values/{dep}.yaml")
+                values = yaml.load(file)
+
+                ingress = values['ingress']
+
+                ingress['ingressClassName'] = control_plane_cfg.ingress.cls if control_plane_cfg.ingress.cls not in ["new", ""] else "nginx"
+                ingress['hosts'][0] = f"{dep}.{global_cfg.domain_name}"
+
+                if control_plane_cfg.cert_manager.external:
+                    ingress['tls'] = []
+                else:
+                    ingress['tls'][0] = {
+                        'secretName': f"devzero-{dep}-tls",
+                        'hosts': [f"{dep}.{global_cfg.domain_name}"],
+                    }
+
+                yaml.dump(values, file)
+
+                sh.make(f"install-{dep}", _cwd=control_plane_deps_dir)
+        except sh.ErrorReturnCode as err:
+            click.echo(f"Error installing dependencies: {err.stderr.decode('utf-8')}", err=True)
+            self.error("DEPS_INSTALL_FAILED")
+    success("Control plane dependencies installed successfully")
